7d5b15c6ee307e5a18e2b5a3e394e5867f0e4e4a
AnthonyMejia0 <mejia.anthony96@gmail.com>
2019-04-10 16:34:29 -0700

    lab4_part2

---
+++ CS120B_lab4/ameji012_lab4_part2/main.c
@@ -0,0 +1,22 @@
+/*	Partner 1 Name & E-mail: Anthony Mejia, ameji012@ucr.edu
+ *	Partner 2 Name & E-mail:
+ *	Lab Section: 22
+ *	Assignment: Lab #4  Exercise #2 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
