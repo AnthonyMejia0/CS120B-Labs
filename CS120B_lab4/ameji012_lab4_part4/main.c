721ed861b2d1e10cc126eeed2f400249cdb91fb5
AnthonyMejia0 <mejia.anthony96@gmail.com>
2019-04-11 15:56:24 -0700

    lab4_part4

---
+++ CS120B_lab4/ameji012_lab4_part4/main.c
@@ -0,0 +1,116 @@
+/*
+ * ameji012_lab4_part4.c
+ *
+ * Created: 4/11/2019 3:31:57 PM
+ * Author : Antho
+ */ 
+
+#include <avr/io.h>
+
+enum States{Start, Lock, Wait_Y, Unlock, Unlock_Hashtag}state;
+unsigned char X;
+unsigned char Y;
+unsigned char hashtag;
+unsigned char lock_button;
+
+void tick() {
+	switch(state) {
+		case Start:
+			state = Lock;
+			break;
+		
+		case Lock:
+			if (hashtag && !X && !Y && !lock_button) {
+				state = Wait_Y;
+			}
+			else {
+				state = Lock;
+			}
+			break;
+		
+		case Wait_Y:
+			if (hashtag && !X && !Y && !lock_button) {
+				state = Wait_Y;
+			}
+			else if (Y && !X && !lock_button && !hashtag){
+				state = Unlock;
+			}
+			else {
+				state = Lock;
+			}
+			break;
+		
+		case Unlock:
+			if (lock_button && !X && !Y && !hashtag){
+				state = Lock;
+			}
+			else if (hashtag && !X && !Y && !lock_button) {
+				state = Unlock_Hashtag;
+			}
+			else {
+				state = Unlock;
+			}
+			break;
+		
+		case Unlock_Hashtag:
+			if (lock_button && !X && !Y && !hashtag) {
+				state = Lock;
+			}
+			else if (Y && !X && !hashtag && !lock_button) {
+				state = Lock;
+			}
+			else if (hashtag && !X && !Y && !lock_button) {
+				state = Unlock_Hashtag;
+			}
+			else {
+				state = Unlock;
+			}
+			break;
+		
+		default:
+			state = Start;
+			break;
+	}
+	
+	switch(state) {
+		case Start:
+			break;
+		
+		case Lock:
+			PORTB = 0x00;
+			PORTC = 0;
+			break;
+		
+		case Wait_Y:
+			PORTB = 0x00;
+			PORTC = 0x01;
+			break;
+		
+		case Unlock:
+			PORTB = 0x01;
+			PORTC = 0x02;
+			break;
+			
+		case Unlock_Hashtag:
+			PORTB = 0x01;
+			PORTC = 0x04;
+			break;
+	}
+}
+
+int main(void)
+{
+	DDRA = 0x78; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+	state = Start;
+
+	while (1)
+	{
+		X = PINA & 0x01;
+		Y = (PINA & 0x02)>>1;
+		hashtag = (PINA & 0x04)>>2;
+		lock_button = (PINA & 0x80)>>7;
+		tick();
+	}
+}
