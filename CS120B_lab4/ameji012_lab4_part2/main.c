b86597c86bd7c17f016bd2735edb329974c56347
AnthonyMejia0 <mejia.anthony96@gmail.com>
2019-04-11 14:40:45 -0700

    lab4_part2

--- CS120B_lab4/ameji012_lab4_part2/main.c
+++ CS120B_lab4/ameji012_lab4_part2/main.c
@@ -11,12 +11,110 @@
 
 #include <avr/io.h>
 
+enum States{Start, init, Increment, Decrement, Reset}state;
+unsigned char currentValue;
+unsigned char A0_button;
+unsigned char A1_button;
+
+void tick() {
+	switch(state) {
+		case Start:
+			state = init;
+			break;
+			
+		case init:
+			if (A0_button && !A1_button) {
+				state = Increment;
+			}
+			else if (!A0_button && A1_button) {
+				state = Decrement;
+			}
+			else if (A0_button && A1_button) {
+				state = Reset;
+			}
+			break;
+			
+		case Increment:
+			if (A0_button && !A1_button) {
+				state = Increment;
+			}
+			else if (!A0_button && A1_button) {
+				state = Decrement;
+			}
+			else if (A0_button && A1_button) {
+				state = Reset;
+			}
+			break;
+			
+		case Decrement:
+			if (A0_button && !A1_button) {
+				state = Increment;
+			}
+			else if (!A0_button && A1_button) {
+				state = Decrement;
+			}
+			else if (A0_button && A1_button) {
+				state = Reset;
+			}
+			break;
+			
+		case Reset:
+			if (A0_button && !A1_button) {
+				state = Increment;
+			}
+			else if (!A0_button && A1_button) {
+				state = Decrement;
+			}
+			else if (A0_button && A1_button) {
+				state = Reset;
+			}
+			break;
+			
+		default:
+			state = Start;
+			break;
+	}
+	
+	switch(state) {
+		case Start:
+			break;
+			
+		case init:
+			PORTC = currentValue = 7;
+			break;
+			
+		case Increment:
+			if (currentValue < 9) {
+				++currentValue;
+				PORTC = currentValue;
+			}
+			break;
+			
+		case Decrement:
+			if (currentValue > 0) {
+				--currentValue;
+				PORTC = currentValue;
+			}
+			break;
+			
+		case Reset:
+			currentValue = 0;
+			PORTC = currentValue;
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRA = 0xFC; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	PORTC = 0x00;
+	state = Start;
+	
     while (1) 
     {
-    }
+		A0_button = PINA & 0x01;
+		A1_button = (PINA & 0x02)>>1;
+		tick();
+   }
 }
 
